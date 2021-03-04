06974b3c8253c3d75fcdcb86c7213d518f66e85c
glauc0 <glaucoten@yahoo.com.br>
2021-03-04 00:28:05 -0300

    Clique/Fragment/Idioma fixo/Icones

--- hello-world-ui5/webapp/controller/Main.controller.js
+++ hello-world-ui5/webapp/controller/Main.controller.js
@@ -1,15 +1,45 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-	function (Controller) {
-		"use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, MessageToast, Fragment) {
+    "use strict";
 
-		return Controller.extend("eggo.helloworldui5.controller.Main", {
-			onInit: function () {
+    return Controller.extend("eggo.helloworldui5.controller.Main", {
+      onInit: function () {},
 
-			}
-		});
-	});
+      btnHelloClick: function () {
+        var lvText = this.getView().byId("helloLabelInput").getValue();
+
+        if (lvText == "") {
+          var oBundle = this.getView().getModel("i18n").getResourceBundle();
+          lvText = oBundle.getText("msgW001");
+        }
+        MessageToast.show(lvText);
+        this.getView().byId("helloLabelInput").setValue("");
+      },
+      btnFragmentClick: function () {
+        var oView = this.getView();
+
+        // create the dialog
+        if (!this.byId("fragmentDialog")) {
+          Fragment.load({
+            id: oView.getId(),
+            name: "eggo.helloworldui5.view.BtnFragment",
+            controller: this,
+          }).then(function (oDialog) {
+            // connect dialog to the root view of this component (models, lifecycle)
+            oView.addDependent(oDialog);
+            oDialog.open();
+          });
+        } else {
+          this.byId("fragmentDialog").open();
+        }
+      },
+      btnCloseFragmentClick: function () {
+		  this.byId("fragmentDialog").close();
+	  },
+    });
+  }
+);
